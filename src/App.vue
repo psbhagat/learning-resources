31b9e64c2ecc7dd4533aded433bb409f02ee8670
[Priyanka Bhagat] <[p.bhagat0312@gmail.com]>
2021-04-07 15:40:00 +0530

    First commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <the-header title="Learning Resources"></the-header>
+  <the-resources></the-resources>
+</template>
+
+<script>
+import TheHeader from './components/Layouts/TheHeader.vue';
+import TheResources from './components/LearningResources/TheResources';
+export default {
+  components: {
+    TheHeader,
+    TheResources,
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ src/components/LearningResources/TheResources.vue
@@ -0,0 +1,84 @@
+<template>
+  <base-card>
+    <base-button
+      @click="setSelectedTab('stored-resources')"
+      :mode="storedResButtonMode"
+      >Stored Resources</base-button
+    >
+    <base-button
+      @click="setSelectedTab('add-resource')"
+      :mode="addResButtonMode"
+      >Add Resource</base-button
+    >
+  </base-card>
+  <keep-alive>
+    <component :is="selectedTab"></component>
+  </keep-alive>
+</template>
+
+<script>
+import StoredResources from './StoredResources.vue';
+import AddResource from './AddResource.vue';
+
+export default {
+  components: {
+    StoredResources,
+    AddResource,
+  },
+  data() {
+    return {
+      selectedTab: 'stored-resources',
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation.',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.org',
+        },
+      ],
+    };
+  },
+  provide() {
+    return {
+      resources: this.storedResources,
+      addResource: this.addResources,
+      removeResources: this.removeResources,
+    };
+  },
+  computed: {
+    storedResButtonMode() {
+      return this.selectedTab === 'stored-resources' ? null : 'flat';
+    },
+    addResButtonMode() {
+      return this.selectedTab === 'add-resource' ? null : 'flat';
+    },
+  },
+  methods: {
+    setSelectedTab(tab) {
+      this.selectedTab = tab;
+    },
+
+    addResources(title, desc, link) {
+      const res = {
+        id: title,
+        title: title,
+        description: desc,
+        link: link,
+      };
+      this.storedResources.unshift(res);
+      this.selectedTab = 'stored-resources';
+    },
+
+    removeResources(resId) {
+      const res = this.storedResources.find((res) => res.id === resId);
+      this.storedResources.splice(res, 1);
+    },
+  },
+};
+</script>
